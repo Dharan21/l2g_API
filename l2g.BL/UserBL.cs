3be2e2705157574695fe58a7b34c9e54e938d066
Bhavya-Shah <2679.stkabirnav@gmail.com>
2020-04-09 17:33:20 +0530

    forgot-password logic

---
+++ l2g.BL/UserBL.cs
@@ -0,0 +1,45 @@
+﻿using l2g.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace l2g.BL
+{
+    public class UserBL
+    {
+        public async Task SendEmail(string email) 
+        {
+            UserDL userDL = new UserDL();
+            var users = userDL.GetUser();
+
+            if (users.Where(s=>s.Email==email).Any())
+            {
+                //send password in the mail
+                var message = new MailMessage();
+                message.To.Add(email);
+                message.From = new MailAddress("Bhavya Shah <bhavya0598@gmail.com>");
+                message.Subject = "Email Verification";
+                message.Body = "Your Password: " + users.Where(s => s.Email == email).First().Password;
+                message.IsBodyHtml = true;
+                using (var smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential("bhavya0598@gmail.com", "lead2ordergenerate");
+                    smtp.EnableSsl = true;
+                    try
+                    {
+                        await smtp.SendMailAsync(message);
+                    }
+                    catch (Exception ex) 
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ l2g/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using l2g.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace l2g.Controllers
+{
+    public class UserController : ApiController
+    {
+        [EnableCors("*", "*", "*")]
+        public async Task<IHttpActionResult> GetPassword(string email)
+            {
+            //string email = "bhavya0598@gmail.com";
+            UserBL userBL = new UserBL();
+            await userBL.SendEmail(email);
+            return Ok();
+        }
+    }
+}
